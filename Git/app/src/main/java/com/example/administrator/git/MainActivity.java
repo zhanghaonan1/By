ed7dfe025e9bb26b03bf6d3055270abd77a12adc
Administrator <6666>
2016-09-08 10:42:08 +0800

    sss

---
+++ Git/app/src/main/java/com/example/administrator/git/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//水水水水水
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        setContentView(R.layout.activity_main);
+//kkjjjjjjjjjjxxxxxssssssssssssssssss
+        //哈哈哈哈哈哈哈哈
+    }
+}
